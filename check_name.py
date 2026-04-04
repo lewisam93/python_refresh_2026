6064bdc6e22b8c9a909107b051220ae03ef29c99
Andrew Lewis <lewisam93@gmail.com>
2026-04-04 11:52:54 -0500

    Initial commit

---
+++ check_name.py
@@ -0,0 +1,7 @@
+name = input("What's your name? >")
+if name == "Peter":
+    print('Hi, Peter. Access granted.')
+elif name == "Andrew":
+    print('Hi, Andrew. Access granted.')
+else:
+    print("Access Denied.")
